a9be619cd0038bc28d617a15b3f75ee73bd877c6
FatemaMajid <fatemamajid2002@gmail.com>
2024-11-07 22:45:09 +0300

    Commit message for additional changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+  server: {
+    port: 3001, // Set your preferred port here
+  },
+
+
 })
+
